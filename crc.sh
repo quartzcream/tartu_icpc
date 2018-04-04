a6bbcf6263b1db7b25d1e8191a6b4a1b6290a561
Oliver Nisumaa <oliver.nisumaa@gmail.com>
2018-04-04 19:24:19 +0300

    added instructions for crc

--- crc.sh
+++ crc.sh
@@ -1,16 +1,14 @@
-#!escape \section{crc.sh}
+# !escape \section{crc.sh}
 
-#!begin_codebook
-#//!start
-#!/bin/env bash
+# !begin_codebook
+#!/bin/envbash
 starts=($(sed '/^\s*$/d' $1 | grep -n "//\!start" | cut -f1 -d:))
 finishes=($(sed '/^\s*$/d' $1 | grep -n "//\!finish" | cut -f1 -d:))
 for ((i=0;i<${#starts[@]};i++)); do
   for j in `seq 10 10 $((finishes[$i]-starts[$i]+8))`; do
     sed '/^\s*$/d' $1 | head -$((finishes[$i]-1)) | tail -$((finishes[$i]-starts[$i]-1)) | \
       head -$j | tr -d '[[:space:]]' | cksum | cut -f1 -d ' ' | tail -c 4
-  done
-  echo
-done "//check last number in each block
-#//!finish
-#!end_codebook
+  done #whistespaces don't matter
+  echo #there shouldn't be any comments in the checked range
+done #//check last number in each block
+# !end_codebook
