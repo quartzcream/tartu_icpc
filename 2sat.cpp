258f5cf2c7b037fbb3dc0d4fc9b3db1c4bf38078
Oliver-Matis Lill <olivermatislill@gmail.com>
2018-04-06 23:59:40 +0300

    Added 2-SAT, Treap and Lazy Segment Tree

---
+++ 2sat.cpp
@@ -0,0 +1,114 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+
+//!escape \section{2-Sat $\mathcal{O}(n)$}
+//!begin_codebook
+//!start
+struct Graph {
+    int n;
+    vector<vector<int> > conn;
+    
+    Graph(int nsize) {
+        n = nsize;
+        conn.resize(n);
+    }
+    
+    void add_edge(int u, int v) {
+        conn[u].push_back(v);
+    }
+    
+    void _topsort_dfs(int pos, vector<int> &result, vector<bool> &explr, vector<vector<int> > &revconn) {
+        if(explr[pos]) {
+            return;
+        }
+        explr[pos] = true;
+        for(auto next : revconn[pos]) {
+            _topsort_dfs(next, result, explr, revconn);
+        }
+        result.push_back(pos);
+    }
+    
+    vector<int> topsort() {
+        vector<vector<int> > revconn(n);
+        for(int u = 0; u < n; u++) {
+            for(auto v : conn[u]) {
+                revconn[v].push_back(u);
+            }
+        }
+        
+        vector<int> result;
+        vector<bool> explr(n, false);
+        for(int i=0; i < n; i++) {
+            _topsort_dfs(i, result, explr, revconn);
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+    
+    void dfs(int pos, vector<int> &result, vector<bool> &explr) {
+        if(explr[pos]) {
+            return;
+        }
+        explr[pos] = true;
+        for(auto next : conn[pos]) {
+            dfs(next, result, explr);
+        }
+        result.push_back(pos);
+    }
+};
+
+//Solution for: http://codeforces.com/group/PjzGiggT71/contest/221700/problem/C
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    
+    Graph g(2*m);
+    
+    for(int i=0; i<n; i++) {
+        int a, sa, b, sb;
+        cin >> a >> sa >> b >> sb;
+        a--, b--;
+        
+        g.add_edge(2*a + 1 - sa, 2*b + sb);
+        g.add_edge(2*b + 1 - sb, 2*a + sa);
+    }
+    
+    vector<int> state(2*m, 0);
+    {
+        vector<int> order = g.topsort();
+        vector<bool> explr(2*m, false);
+        
+        for(auto u : order) {
+            vector<int> traversed;
+            g.dfs(u, traversed, explr);
+            
+            if(traversed.size() > 0 && !state[traversed[0]^1]) {
+                for(auto c : traversed) {
+                    state[c] = 1;
+                }
+            }
+        }
+    }
+    
+    for(int i=0; i < m; i++) {
+        if(state[2*i] == state[2*i+1]) {
+            cout << "IMPOSSIBLE\n";
+            return 0;
+        }
+    }
+    
+    for(int i=0; i < m; i++) {
+        cout << state[2*i+1] << '\n';
+    }
+    return 0;
+}
+//!finish
+//!end_codebook
+
+
---
+++ lazyst.cpp
@@ -0,0 +1,151 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//!escape \section{Lazy Segment Tree $\mathcal{O}(\log n)$ per query}
+//!begin_codebook
+//!start
+struct SegmentTree {
+    struct Node {
+        long long value = 0;
+        int size = 1;
+        
+        int lazy_add = 0;
+        bool lazy_set = false;
+        int lazy_to_set = 0;
+        
+        void set(int to_set) {
+            lazy_set = true;
+            lazy_to_set = to_set;
+            lazy_add = 0;
+        }
+    };
+    int n;
+    vector<Node> nodes;
+    
+    void propagate(int pos) {
+        Node& cur = nodes[pos];
+        if(cur.lazy_set) {
+            if(pos < n) {
+                nodes[pos*2].set(cur.lazy_to_set);
+                nodes[pos*2+1].set(cur.lazy_to_set);
+            }
+            cur.value = 1LL * cur.size * cur.lazy_to_set;
+            cur.lazy_set = false;
+        }
+        if(cur.lazy_add != 0) {
+            if(pos < n) {
+                nodes[pos*2].lazy_add += cur.lazy_add;
+                nodes[pos*2+1].lazy_add += cur.lazy_add;
+            }
+            cur.value += 1LL * cur.size * cur.lazy_add;
+            cur.lazy_add = 0;
+        }
+    }
+    
+    long long get_value(int pos) {
+        propagate(pos);
+        return nodes[pos].value;
+    }
+    
+    SegmentTree(int nsize) {
+        n = 1;
+        while(n < nsize) {
+            n *= 2;
+        }
+        nodes.resize(2*n);
+        for(int i=n-1; i>0; i--) {
+            nodes[i].size = nodes[2*i].size * 2;
+        }
+    }
+    
+    void set(int l, int r, int to_set, int pos = 1, int lb = 0, int rb = -1) {
+        propagate(pos);
+        if(rb == -1) rb = n;
+        if(l <= lb && rb <= r) {
+            nodes[pos].set(to_set);
+            return;
+        }
+        int mid = (lb + rb) / 2;
+        if(l < mid) {
+            set(l, r, to_set, pos*2, lb, mid);
+        }
+        if(mid < r) {
+            set(l, r, to_set, pos*2+1, mid, rb);
+        }
+        nodes[pos].value = get_value(pos*2) + get_value(pos*2+1);
+    }
+    
+    void add(int l, int r, int to_add, int pos = 1, int lb = 0, int rb = -1) {
+        propagate(pos);
+        if(rb == -1) rb = n;
+        if(l <= lb && rb <= r) {
+            nodes[pos].lazy_add += to_add;
+            return;
+        }
+        int mid = (lb + rb) / 2;
+        if(l < mid) {
+            add(l, r, to_add, pos*2, lb, mid);
+        }
+        if(mid < r) {
+            add(l, r, to_add, pos*2+1, mid, rb);
+        }
+        nodes[pos].value = get_value(pos*2) + get_value(pos*2+1);
+    }
+    
+    long long get(int l, int r, int pos = 1, int lb = 0, int rb = -1) {
+        propagate(pos);
+        if(rb == -1) rb = n;
+        if(l <= lb && rb <= r) {
+            return get_value(pos);
+        }
+        int mid = (lb + rb) / 2;
+        long long result = 0;
+        if(l < mid) {
+            result += get(l, r, pos*2, lb, mid);
+        }
+        if(mid < r) {
+            result += get(l, r, pos*2+1, mid, rb);
+        }
+        return result;
+    }
+};
+
+//Solution for: http://codeforces.com/group/UO1GDa2Gwb/contest/219104/problem/LAZY
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    
+    SegmentTree stree(n);
+    for(int i=0;i<n;i++) {
+        int a;
+        cin >> a;
+        stree.set(i, i+1, a);
+    }
+    
+    for(int i=0;i<m;i++) {
+        int type;
+        cin >> type;
+        
+        if(type == 1) {
+            int l, r, d;
+            cin >> l >> r >> d;
+            stree.add(l-1, r, d);
+        } else if(type == 2) {
+            int l, r, x;
+            cin >> l >> r >> x;
+            stree.set(l-1, r, x);
+        } else {
+            int l, r;
+            cin >> l >> r;
+            cout << stree.get(l-1, r) << '\n';
+        }
+    }
+    
+    return 0;
+}
+//!finish
+//!end_codebook
