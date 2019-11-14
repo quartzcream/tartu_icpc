b2178984065126daf6be23cbf2ba91cb02977251
thuustalu <thuustalu@gmail.com>
2019-11-14 10:32:33 +0200

    final version for NWERC

---
+++ template.cpp
@@ -0,0 +1,33 @@
+//!escape Template
+//!begin_codebook
+//!start
+using namespace std;
+#include <iomanip>
+#include <iostream>
+#include <fstream>
+#ifdef LOCAL
+bool DEBUG = true;
+#else
+bool DEBUG = false;
+#endif
+#define FAST {ios::sync_with_stdio(0);cin.tie(0);cout<<setprecision(10)<<fixed;}
+#define FOR(a,b,c) for(int a=(b); a<(int)(c); ++a)
+#define DB(a) if(DEBUG) cerr<<#a<<" "<<a<< "\n";
+#define all(v) (v).begin(),(v).end()
+using ll = long long;
+int MAXINT=2e9;
+ll MAXLL=9e18;
+// Includes
+#include <vector>
+#include <cmath>
+int main()
+{
+    #ifdef LOCAL
+        ifstream cin("input.txt");
+        ofstream cout("output.txt");
+    #endif
+	FAST
+    //
+}
+//!finish
+//!end_codebook
